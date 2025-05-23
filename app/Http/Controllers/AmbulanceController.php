daf735c85eac79762ba2e532ba86e4b89f9a1f99
subhamroyavi <subhamroy@gmail.com>
2025-05-24 00:12:15 +0530

    ambulance search bar working dynamically but not properly

--- app/Http/Controllers/AmbulanceController.php
+++ app/Http/Controllers/AmbulanceController.php
@@ -10,27 +10,29 @@ class AmbulanceController extends Controller
     public function index(Request $request)
     {
         $search = $request->search;
-        $ambulances = Ambulance::query()->with('location');
+        $ambulances = Ambulance::where('status', '1')->orderBy('created_at', 'DESC')->paginate(6);
+        // $ambulances = Ambulance::query()->with('location');
 
-        if ($search) {
-            $ambulances->where(function ($query) use ($search) {
-                $query->where('first_name', 'like', "%$search%")
-                    ->orWhere('last_name', 'like', "%$search%")
-                    ->orWhere('email', 'like', "%$search%")
-                    ->orWhere('phone', 'like', "%$search%");
-            });
-        } else {
-            $ambulances->where('status', '1');
-        }
+        // if ($search) {
+        //     $ambulances->where(function ($query) use ($search) {
+        //         $query->where('first_name', 'like', "%$search%")
+        //             ->orWhere('last_name', 'like', "%$search%")
+        //             ->orWhere('email', 'like', "%$search%")
+        //             ->orWhere('phone', 'like', "%$search%");
+        //     });
+        // } else {
+        //     $ambulances->where('status', '1');
+        // }
 
-        $ambulances = $ambulances->orderBy('created_at', 'DESC')->paginate(6);
+        // $ambulances = $ambulances->orderBy('created_at', 'DESC')->paginate(6);
 
-        if ($request->ajax()) {
-            return response()->json([
-                'data' => $ambulances->items(),
-                'links' => $ambulances->links()->toHtml()
-            ]);
-        }
+        // if ($request->ajax()) {
+        //     return response()->json([
+        //         'data' => $ambulances->items(),
+        //         'links' => $ambulances->links()->toHtml()
+        //     ]);
+        // }
+        
 
         return view('user_panel.ambulances', compact('ambulances'));
     }
@@ -38,8 +40,8 @@ class AmbulanceController extends Controller
     public function search(Request $request)
     {
         // dd($request->searchStr);
-              
-              
+
+
         $search = $request->searchStr;
         $ambulances = Ambulance::where('first_name', 'like', "%$search%")
             ->orWhere('last_name', 'like', "%$search%")
@@ -54,7 +56,7 @@ class AmbulanceController extends Controller
         } else {
             return response()->json(['status' => 'nothing_found']);
         }
-    
+
 
         $search = $request->searchStr;
         $ambulances = Ambulance::where('first_name', 'like', "%$search%")
